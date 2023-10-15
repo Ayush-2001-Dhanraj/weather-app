416974f1c9abcb5fba97de43ae47b016abed7e6e
Ayush4NattyHatty <dhanrajaayush123@gmail.com>
2023-10-15 10:22:57 +0530

    SafeAreaView + statusbar currentheight

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import { Text, View, SafeAreaView, StyleSheet, StatusBar } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Text>Open up App.js to start working on your app!</Text>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight
+    }
 });
