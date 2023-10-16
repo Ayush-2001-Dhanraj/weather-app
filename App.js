dc63984d12fbc6eb5e2de0908482b3248a89c3b1
Ayush4NattyHatty <dhanrajaayush123@gmail.com>
2023-10-16 18:46:05 +0530

    Loading boilerplate code

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/tabs';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
 
 const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    if (isLoading)
+        return (
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator size={'large'} color={'blue'} />
+            </View>
+        );
+
     return (
         <NavigationContainer>
             <Tabs />
         </NavigationContainer>
     );
 };
+
+const styles = StyleSheet.create({
+    loadingContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    }
+});
 
 export default App;
